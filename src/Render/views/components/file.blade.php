46a0467a396fc9f44a965a214d47ad0a61785f51
Gergo Mando <gergo.mando@gmail.com>
2019-04-22 19:30:09 +0200

    add hidden attribute

--- src/Render/views/components/file.blade.php
+++ src/Render/views/components/file.blade.php
@@ -1,5 +1,11 @@
+<div
+@if(isset($attributes['hidden']) && $attributes['hidden'])
+style="display:none"; 
+@endif
+>
 <fileupload-component
     :input-name="'{{{ $column }}}'"
     :show-preview="true"
 ></fileupload-component>
 @include('form-manager-render::components.validation')
+</div>
--- src/Render/views/components/textarea.blade.php
+++ src/Render/views/components/textarea.blade.php
@@ -1,4 +1,8 @@
-<div class="form-group">
+<div class="form-group"
+@if(isset($attributes['hidden']) && $attributes['hidden'])
+style="display:none"; 
+@endif
+>
     {{ Form::label($column, $label, ['class' => 'control-label']) }}
     {{ Form::textarea($column, $value, array_merge(['class' => 'form-control', 'rows' => 3], $attributes)) }}
     @include('form-manager-render::components.validation')
