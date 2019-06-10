e2917739fdb6fc7e1028c261ccf0b83eb75b989b
Gergo Mando <gergo.mando@gmail.com>
2019-06-10 11:23:59 +0200

    add selected to multiselect

--- src/Render/views/components/multiselect.blade.php
+++ src/Render/views/components/multiselect.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group bmd-form-group">
     {{ Form::label($column, $label ? $label : ' ', ['class' => 'control-label bmd-label-static']) }}
-	<multiselect-component :name="{{{ json_encode($column) }}}" :options="{{{ json_encode($list) }}}"></multiselect-component>
+	<multiselect-component :selected="{{{ json_encode($default) }}} :name="{{{ json_encode($column) }}}" :options="{{{ json_encode($list) }}}"></multiselect-component>
     @include('form-manager-render::components.validation')
 </div>
 
